4ae5931f9cdbf3169c1ef07d8d8dd3529e7b443d
darkz70 <d91313250@gmail.com>
2026-06-10 22:49:42 +0200

    Update SkinTotemCommandManager.java

--- src/main/java/com/darkz/skintotem/client/command/SkinTotemCommandManager.java
+++ src/main/java/com/darkz/skintotem/client/command/SkinTotemCommandManager.java
@@ -6,16 +6,18 @@ import static net.fabricmc.fabric.api.client.command.v2.ClientCommands.literal;
 
 public class SkinTotemCommandManager {
 
-public static void register() {
-    ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
-        dispatcher.register(literal("skintotem")
-                .then(RefreshCommand.getInstance())
-                .then(SkinTotemCommand.getInfoCommand())
-                .then(SkinTotemCommand.getCreditsCommand())
-                .then(SkinTotemCommand.getTlCommand())
-                .then(SkinTotemCommand.getElyCommand())
-                .then(SkinTotemCommand.getUrlCommand())
-                .executes(SkinTotemCommand.getHelpExecutor())
-        );
-    });
+    public static void register() {
+        ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+            dispatcher.register(literal("skintotem")
+                    .then(RefreshCommand.getInstance())
+                    .then(SkinTotemCommand.getInfoCommand())
+                    .then(SkinTotemCommand.getCreditsCommand())
+                    .then(SkinTotemCommand.getTlCommand())
+                    .then(SkinTotemCommand.getElyCommand())
+                    .then(SkinTotemCommand.getUrlCommand())
+                    .executes(SkinTotemCommand.getHelpExecutor())
+            );
+        });
+    }
+
 }
