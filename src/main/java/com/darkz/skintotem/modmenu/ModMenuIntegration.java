52a22b2762321e19fbe884902396045765fc83cf
darkz70 <d91313250@gmail.com>
2026-06-10 12:34:47 +0200

    Update ModMenuIntegration.java

--- src/main/java/com/darkz/skintotem/modmenu/ModMenuIntegration.java
+++ src/main/java/com/darkz/skintotem/modmenu/ModMenuIntegration.java
@@ -1,15 +1,15 @@
 package com.darkz.skintotem.modmenu;
 
-/*? if dep.modmenu {*/
 import com.terraformersmc.modmenu.api.*;
 import net.fabricmc.loader.api.*;
 import com.darkz.skintotem.MyTotemDoll;
 import com.darkz.skintotem.client.MyTotemDollClient;
 import com.darkz.skintotem.yacl.YACLConfigurationScreen;
 
-public class ModMenuIntegration {
+public class ModMenuIntegration implements ModMenuApi {
 
-	public Object getModConfigScreenFactory() {
+	@Override
+	public ConfigScreenFactory<?> getModConfigScreenFactory() {
 		FabricLoader fabricLoader = FabricLoader.getInstance();
 		if (fabricLoader.isModLoaded("yet_another_config_lib_v3")) {
 			ModContainer modContainer = fabricLoader.getModContainer("yet_another_config_lib_v3").orElseThrow();
@@ -17,18 +17,13 @@ public class ModMenuIntegration {
 			try {
 				Version requestsVersion = Version.parse(MyTotemDoll.YACL_DEPEND_VERSION);
 				if (version.compareTo(requestsVersion) >= 0) {
-					return (com.terraformersmc.modmenu.api.ConfigScreenFactory<?>) YACLConfigurationScreen::createScreen;
+					return YACLConfigurationScreen::createScreen;
 				}
 			} catch (VersionParsingException e) {
 				MyTotemDollClient.LOGGER.error("Failed to compare YACL version, tell mod author about this error: ", e);
 			}
-			return (com.terraformersmc.modmenu.api.ConfigScreenFactory<?>) (parent -> NoConfigLibraryScreen.createScreenAboutOldVersion(parent, version.getFriendlyString()));
+			return parent -> NoConfigLibraryScreen.createScreenAboutOldVersion(parent, version.getFriendlyString());
 		}
-		return (com.terraformersmc.modmenu.api.ConfigScreenFactory<?>) NoConfigLibraryScreen::createScreen;
+		return NoConfigLibraryScreen::createScreen;
 	}
 }
-/*?} else {*/
-/*
-public class ModMenuIntegration {}
-*/
-/*?}*/
