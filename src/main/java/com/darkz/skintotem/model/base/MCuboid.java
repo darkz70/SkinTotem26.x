f0357d9219496276262166df2648af8c6892cac3
darkz70 <d91313250@gmail.com>
2026-06-12 19:51:09 +0200

    Update MCuboid.java

--- src/main/java/com/darkz/skintotem/model/base/MCuboid.java
+++ src/main/java/com/darkz/skintotem/model/base/MCuboid.java
@@ -16,8 +16,6 @@ public class MCuboid extends ModelPart.Cube {
 
 	public MCuboid(Vector3f pos, Vector3f size, Polygon[] quads, CubeDeformation dilation) {
 		super(0, 0, pos.x(), pos.y(), pos.z(), size.x(), size.y(), size.z(), 0, 0, 0, false, 0, 0, EMPTY_SET);
-		// In 26.1 polygons is final, so we use the mixin accessor to overwrite it
-		((CubePolygonsAccessor) (Object) this).setPolygons(quads);
 		// this.polygons = quads; // In 26.1 polygons is final and set in super constructor
 		// We might need a different approach if we really need to override polygons, 
 		// but for now let's see if the super constructor is enough.
