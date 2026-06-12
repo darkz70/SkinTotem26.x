5bf68fe4945f559c71d54db5a0761e468bf4e6dd
darkz70 <d91313250@gmail.com>
2026-06-12 18:34:35 +0200

    Update MCuboid.java

--- src/main/java/com/darkz/skintotem/model/base/MCuboid.java
+++ src/main/java/com/darkz/skintotem/model/base/MCuboid.java
@@ -2,6 +2,7 @@ package com.darkz.skintotem.model.base;
 
 import java.util.Set;
 import lombok.Getter;
+import com.darkz.skintotem.mixin.accessor.CubePolygonsAccessor;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.model.geom.ModelPart.*;
 import net.minecraft.client.model.geom.builders.CubeDeformation;
@@ -16,10 +17,8 @@ public class MCuboid extends ModelPart.Cube {
 
 	public MCuboid(Vector3f pos, Vector3f size, Polygon[] quads, CubeDeformation dilation) {
 		super(0, 0, pos.x(), pos.y(), pos.z(), size.x(), size.y(), size.z(), 0, 0, 0, false, 0, 0, EMPTY_SET);
-		// this.polygons = quads; // In 26.1 polygons is final and set in super constructor
-		// We might need a different approach if we really need to override polygons, 
-		// but for now let's see if the super constructor is enough.
-		// If not, we might need to use a Mixin to make it non-final or use Reflection.
+		// In 26.1 polygons is final, so we use the mixin accessor to overwrite it
+		((CubePolygonsAccessor) (Object) this).setPolygons(quads);
 		this.dilation = dilation;
 	}
 
