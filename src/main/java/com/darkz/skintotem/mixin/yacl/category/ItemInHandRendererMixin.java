016f3ddb2836f7c43f657e881cd6b94b293f3c74
darkz69i42k <d91313250@gmail.com>
2026-06-20 18:46:48 +0200

    Update ItemInHandRendererMixin.java

--- src/main/java/com/darkz/skintotem/mixin/yacl/category/ItemInHandRendererMixin.java
+++ src/main/java/com/darkz/skintotem/mixin/yacl/category/ItemInHandRendererMixin.java
@@ -19,7 +19,6 @@ import net.minecraft.world.item.*;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.Coerce;
 
 @Mixin(ItemInHandRenderer.class)
 public class ItemInHandRendererMixin {
@@ -27,7 +26,7 @@ public class ItemInHandRendererMixin {
 	@Unique
 	private static void createBoolean(LocalBooleanRef ref) {
 		Minecraft client = Minecraft.getInstance();
-		Screen currentScreen = client.screen;
+		Screen currentScreen = client.gui.screen();
 
 		ref.set(false);
 		if (YACLConfigurationScreen.notOpen(currentScreen)) {
@@ -62,9 +61,25 @@ public class ItemInHandRendererMixin {
 		draw.accept(original);
 	}
 
+	@Unique
+	private static Object RENDER_BOTH_HANDS;
+
+	@Unique
+	private static Object getRenderBothHands() {
+		if (RENDER_BOTH_HANDS == null) {
+			try {
+				Class<?> handRenderSelectionClass = Class.forName("net.minecraft.client.renderer.ItemInHandRenderer$HandRenderSelection");
+				RENDER_BOTH_HANDS = handRenderSelectionClass.getField("RENDER_BOTH_HANDS").get(null);
+			} catch (ReflectiveOperationException e) {
+				throw new RuntimeException("SkinTotem: failed to resolve ItemInHandRenderer.HandRenderSelection.RENDER_BOTH_HANDS via reflection", e);
+			}
+		}
+		return RENDER_BOTH_HANDS;
+	}
+
 	@Inject(
 			at = @At("HEAD"),
-			method = "renderHandsWithItems(FLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/SubmitNodeCollector;Lnet/minecraft/client/player/LocalPlayer;I)V"
+			method = "submitHandsWithItems(FLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/SubmitNodeCollector;Lnet/minecraft/client/player/LocalPlayer;I)V"
 	)
 	private void createBoolean(CallbackInfo ci, @Share("mtd_bl") LocalBooleanRef ref) {
 		createBoolean(ref);
@@ -72,33 +87,19 @@ public class ItemInHandRendererMixin {
 
 	@WrapOperation(
 			at = @At(
-					value = "FIELD",
-					target = "Lnet/minecraft/client/renderer/ItemInHandRenderer$HandRenderSelection;renderMainHand:Z"
+					value = "INVOKE",
+					target = "Lnet/minecraft/client/renderer/ItemInHandRenderer;evaluateWhichHandsToRender(Lnet/minecraft/client/player/LocalPlayer;)Lnet/minecraft/client/renderer/ItemInHandRenderer$HandRenderSelection;"
 			),
-			method = "renderHandsWithItems(FLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/SubmitNodeCollector;Lnet/minecraft/client/player/LocalPlayer;I)V"
+			method = "submitHandsWithItems(FLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/SubmitNodeCollector;Lnet/minecraft/client/player/LocalPlayer;I)V"
 	)
-	private boolean swapRenderValue1(@Coerce Object instance, Operation<Boolean> original, @Share("mtd_bl") LocalBooleanRef ref) {
+	private Object forceRenderBothHands(LocalPlayer player, Operation<Object> original, @Share("mtd_bl") LocalBooleanRef ref) {
 		if (ref.get()) {
-			return true;
+			return getRenderBothHands();
 		}
-		return original.call(instance);
+		return original.call(player);
 	}
 
-	@WrapOperation(
-			at = @At(
-					value = "FIELD",
-					target = "Lnet/minecraft/client/renderer/ItemInHandRenderer$HandRenderSelection;renderOffHand:Z"
-			),
-			method = "renderHandsWithItems(FLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/SubmitNodeCollector;Lnet/minecraft/client/player/LocalPlayer;I)V"
-	)
-	private boolean swapRenderValue2(@Coerce Object instance, Operation<Boolean> original, @Share("mtd_bl") LocalBooleanRef ref) {
-		if (ref.get()) {
-			return true;
-		}
-		return original.call(instance);
-	}
-
-	@WrapOperation(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/ItemInHandRenderer;renderArmWithItem(Lnet/minecraft/client/player/AbstractClientPlayer;FFLnet/minecraft/world/InteractionHand;FLnet/minecraft/world/item/ItemStack;FLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/SubmitNodeCollector;I)V"), method = "renderHandsWithItems(FLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/SubmitNodeCollector;Lnet/minecraft/client/player/LocalPlayer;I)V")
+	@WrapOperation(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/ItemInHandRenderer;submitArmWithItem(Lnet/minecraft/client/player/AbstractClientPlayer;FFLnet/minecraft/world/InteractionHand;FLnet/minecraft/world/item/ItemStack;FLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/SubmitNodeCollector;I)V"), method = "submitHandsWithItems(FLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/SubmitNodeCollector;Lnet/minecraft/client/player/LocalPlayer;I)V")
 	private void swapRenderingStack(ItemInHandRenderer instance, AbstractClientPlayer playerEntity, float a, float b, InteractionHand hand, float c, ItemStack stack, float d, PoseStack matrixStack, SubmitNodeCollector queue, int i, Operation<Void> original, @Share("mtd_bl") LocalBooleanRef ref) {
 		Consumer<ItemStack> consumer = (itemStack) -> original.call(instance, playerEntity, a, b, hand, c, itemStack, d, matrixStack, queue, i);
 		if (ref.get()) {
